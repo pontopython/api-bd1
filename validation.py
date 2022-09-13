f97a4bd8359b3d40e2df2f6dc1c3fe64849ac599
Larissa Reis <laliffr@gmail.com>
2022-09-12 21:09:30 -0300

    mudei nomes e add opcao de mostrar ou nao o erro

--- validation.py
+++ validation.py
@@ -6,7 +6,7 @@ def has_name_valid_characters(name):
     return re.match('^[a-zA-Z0-9_]+$', name)
 
 
-def is_name_valid(name):
+def is_name_valid(name, show=False):
     """
     Retorna True ou False. Verifica se o nome é valido ou não.
     Validações:
@@ -15,19 +15,22 @@ def is_name_valid(name):
         - não pode conter caracteres especiais, exceto underline "_"
     """
     if len(name) <= 2:
-        print('O nome deve ter mais de 2 caracteres. Tente novamente!')
+        if show == True:
+            print('O nome deve ter mais de 2 caracteres. Tente novamente!')
         return False
     elif not has_name_valid_characters(name):
-        print('Formato inválido. Tente novamente!')
+        if show == True:
+            print('Formato inválido. Tente novamente!')
         return False
     elif re.match('\d', name[0]):
-        print('Nomes não podem começar com número')
+        if show == True:
+            print('Nomes não podem começar com número')
         return False
     else:
         return True
 
 
-def validate_username():
+def prompt_for_valid_username():
     """
     Loop pedindo para o usuário inserir o nome caso
     o nome seja inválido.
@@ -44,7 +47,7 @@ def is_email_valid(email):
     return re.match('^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$', email)
 
 
-def validate_email():
+def prompt_for_valid_email():
     input_email = input('Digite o email: ')
 
     while not is_email_valid(input_email):
@@ -54,7 +57,7 @@ def validate_email():
     return input_email
 
 
-def validate_team_name():
+def prompt_for_valid_team_name():
     input_team_name = input('Digite o nome do time: ')
 
     while not is_name_valid(input_team_name):
@@ -65,9 +68,9 @@ def validate_team_name():
 
 if __name__ == '__main__':
     # test
-    name = validate_username()
-    email = validate_email()
-    team = validate_team_name()
+    name = prompt_for_valid_username()
+    email = prompt_for_valid_email()
+    team = prompt_for_valid_team_name()
     print(name)
     print(email)
     print(team)
