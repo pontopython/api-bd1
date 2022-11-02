0c3602a4405905b8b66015e31ab646c781718e0e
larissa-fernanda <laliffr@gmail.com>
2022-11-02 15:44:33 -0300

    add função de achar a sprint aberta

--- api/sprints/sprints.py
+++ api/sprints/sprints.py
@@ -3,18 +3,31 @@ from ..turmas.tui import select_leader_group
 from ..teams.tui import select_team
 from .repository import create_sprint, search_sprint_by, update_sprint
 
-def has_opened_sprint(team_id):
-    sprints = search_sprint_by("team_id", team_id)
-    opened_sprints = [sprint for sprint in sprints if sprint['status'] == 'aberta']
 
-    return len(opened_sprints) > 0
+def get_opened_sprint(team_id):
+    sprints = search_sprint_by("team_id", team_id)
+    opened_sprints = [
+        sprint for sprint in sprints if sprint['status'] == 'aberta']
+
+    if len(opened_sprints) > 0:
+        return opened_sprints[0]
+
+    return None
+        
+
+
+def has_opened_sprint(team_id):
+    opened_sprints = get_opened_sprint(team_id)
+
+    return opened_sprints is not None
+
 
 def select_sprint_tui(team_id):
     sprints = search_sprint_by("team_id", team_id)
-    
+
     for index, sprint in enumerate(sprints):
         print(f'{index + 1}. {sprint["id"]} - {sprint["status"]}')
-    
+
     input_sprint = int(input("Qual sprint deseja selecionar? "))
 
     if input_sprint > 0 and input_sprint <= len(sprints):
@@ -41,6 +54,7 @@ def close_sprint_tui():
     selected_sprint['status'] = 'fechada'
     update_sprint('id', selected_sprint['id'], selected_sprint)
 
+
 def create_sprint_tui():
     user = get_logged_user()
     id = user['id']
@@ -60,6 +74,7 @@ def create_sprint_tui():
 
     sprint = create_sprint(selected_team['id'])
     return sprint
+
 
 if __name__ == '__main__':
     create_sprint_tui()
